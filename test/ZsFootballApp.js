e076fe22e94c92db3d52981bd83c657f87f537cf
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-05-06 07:38:51 +0200

    Adds test file

---
+++ test/ZsFootballApp.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import ZsFootballApp from '../src/ZsFootballApp';
+
+describe('ZsFootballApp', function() {
+	it('should be tested', function() {
+		assert.fail('No tests for this module yet.');
+	});
+});
