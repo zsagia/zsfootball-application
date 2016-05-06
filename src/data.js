4048b0862802bbffd230f8df279741542510037b
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-05-06 16:19:25 +0200

    Refactoring date loading

---
+++ src/data.js
@@ -0,0 +1,674 @@
+var matches = [
+	[
+	  {
+	    id: 7101,
+	    attendance: 75261,
+	    awayClub: 'Tottenham Hotspurs',
+	    awayGoals: 0,
+	    homeClub: 'Manchester United',
+	    homeGoals: 1,
+	    matchDate: 1439041500000,
+	    round: 1
+	  },  
+	  {
+	    id: 7102,
+	    attendance:  11155,
+	    awayClub: 'Aston Villa',
+	    awayGoals: 1,
+	    homeClub: 'AFC Bournemouth',
+	    homeGoals: 0,
+	    matchDate: 1439046000000,
+	    round: 1
+	  },
+	  {
+	    id: 7103,
+	    attendance: 27036,
+	    awayClub: 'Crystal Palace',
+	    awayGoals: 3,
+	    homeClub: 'Norwich City',
+	    homeGoals: 1,
+	    matchDate: 1439046000000,
+	    round: 1
+	  },
+	  {
+	    id: 7104,
+	    attendance: 39063,
+	    awayClub: 'Watford FC',
+	    awayGoals: 2,
+	    homeClub: 'Everton',
+	    homeGoals: 2,
+	    matchDate: 1439046000000,
+	    round: 1
+	  },
+	  {
+	    id: 7105,
+	    attendance: 32242,
+	    awayClub: 'Sunderland AFC',
+	    awayGoals: 2,
+	    homeClub: 'Leicester City',
+	    homeGoals: 4,
+	    matchDate: 1439046000000,
+	    round: 1
+	  },
+	  {
+	    id: 7106,
+	    attendance: 41232,
+	    awayClub: 'Swansea City',
+	    awayGoals: 2,
+	    homeClub: 'Chelsea FC',
+	    homeGoals: 2,
+	    matchDate: 1439055000000,
+	    round: 1
+	  },
+	  {
+	    id: 7107,
+	    attendance: 59996,
+	    awayClub: 'West Ham',
+	    awayGoals: 2,
+	    homeClub: 'Arsenal',
+	    homeGoals: 0,
+	    matchDate: 1439127000000,
+	    round: 1
+	  },
+	  {
+	    id: 7108,
+	    attendance: 59996,
+	    awayClub: 'Southampton',
+	    awayGoals: 2,
+	    homeClub: 'Newcastle United',
+	    homeGoals: 2,
+	    matchDate: 1439127000000,
+	    round: 1
+	  },
+	  {
+	    id: 7109,
+	    attendance: 27654,
+	    awayClub: 'Liverpool FC',
+	    awayGoals: 1,
+	    homeClub: 'Stoke City',
+	    homeGoals: 0,
+	    matchDate: 1439136000000,
+	    round: 1
+	  },
+	  {
+	    id: 7110,
+	    attendance: 24564,
+	    awayClub: 'Manchester City',
+	    awayGoals: 3,
+	    homeClub: 'West Brom',
+	    homeGoals: 0,
+	    matchDate: 1439236800000,
+	    round: 1
+	  }
+	],
+	[
+	    {
+	      id: 7111,
+	      attendance: 42200,
+	      awayClub: 'Manchester United',
+	      awayGoals: 1,
+	      homeClub: 'Aston Villa',
+	      homeGoals: 0,
+	      matchDate: 1439581500000,
+	      round: 2
+	    },
+	    {
+	      id: 7112,
+	      attendance: 36004,
+	      awayClub: 'Stoke City',
+	      awayGoals: 2,
+	      homeClub: 'Tottenham Hotspurs',
+	      homeGoals: 2,
+	      matchDate: 1439650800000,
+	      round: 2
+	    },
+	    {
+	      id: 7113,
+	      attendance: 41379,
+	      awayClub: 'Norwich City',
+	      awayGoals: 3,
+	      homeClub: 'Sunderland AFC',
+	      homeGoals: 1,
+	      matchDate: 1439650800000,
+	      round: 2
+	    },
+	    {
+	      id: 7114,
+	      attendance: 20678,
+	      awayClub: 'Newcastle United',
+	      awayGoals: 0,
+	      homeClub: 'Swansea City',
+	      homeGoals: 2,
+	      matchDate: 1439650800000,
+	      round: 2
+	    },
+	    {
+	      id: 7115,
+	      attendance: 34857,
+	      awayClub: 'Leicester City',
+	      awayGoals: 2,
+	      homeClub: 'West Ham',
+	      homeGoals: 1,
+	      matchDate: 1439650800000,
+	      round: 2
+	    },
+	    {
+	      id: 7116,
+	      attendance: 20011,
+	      awayClub: 'West Brom',
+	      awayGoals: 0,
+	      homeClub: 'Watford FC',
+	      homeGoals: 0,
+	      matchDate: 1439650800000,
+	      round: 2
+	    },
+	    {
+	      id: 7117,
+	      attendance: 30966,
+	      awayClub: 'Everton',
+	      awayGoals: 3,
+	      homeClub: 'Southampton',
+	      homeGoals: 0,
+	      matchDate: 1439642700000,
+	      round: 2
+	    },
+	    {
+	      id: 7118,
+	      attendance: 54331,
+	      awayClub: 'Chelsea FC',
+	      awayGoals: 0,
+	      homeClub: 'Manchester City',
+	      homeGoals: 3,
+	      matchDate: 1439740800000,
+	      round: 2
+	    },
+	    {
+	      id: 7119,
+	      attendance: 24732,
+	      awayClub: 'Arsenal',
+	      awayGoals: 2,
+	      homeClub: 'Crystal Palace',
+	      homeGoals: 1,
+	      matchDate: 1439731800000,
+	      round: 2
+	    },
+	    {
+	      id: 7120,
+	      attendance: 44102,
+	      awayClub: 'AFC Bournemouth',
+	      awayGoals: 0,
+	      homeClub: 'Liverpool FC',
+	      homeGoals: 1,
+	      matchDate: 1439841600000,
+	      round: 2
+	    }
+	],
+	[
+		{
+		  id: 7121,
+		  attendance: 26771,
+		  awayClub: 'Stoke City',
+		  awayGoals: 1,
+		  homeClub: 'Norwich City',
+		  homeGoals: 1,
+		  matchDate: 1440255600000,
+		  round: 3,
+		  referee: 'Mike Dean'
+		},
+		{
+		  id: 7122,
+		  attendance: 39198,
+		  awayClub: 'Swansea City',
+		  awayGoals: 1,
+		  homeClub: 'Sunderland AFC',
+		  homeGoals: 1,
+		  matchDate: 1440255600000,
+		  round: 3,
+		  referee: 'Neil Swarbrick'
+		},
+		{
+		  id: 7123,
+		  attendance: 39198,
+		  awayClub: 'Tottenham Hotspurs',
+		  awayGoals: 1,
+		  homeClub: 'Leicester City',
+		  homeGoals: 1,
+		  matchDate: 1440255600000,
+		  round: 3,
+		  referee: 'Neil Swarbrick'
+		},
+		{
+		  id: 7124,
+		  attendance: 39198,
+		  awayClub: 'AFC Bournemouth',
+		  awayGoals: 4,
+		  homeClub: 'West Ham',
+		  homeGoals: 3,
+		  matchDate: 1440255600000,
+		  round: 3,
+		  referee: 'Neil Swarbrick'
+		},
+		{
+		  id: 7125,
+		  attendance: 39198,
+		  awayClub: 'Aston Villa',
+		  awayGoals: 1,
+		  homeClub: 'Crystal Palace',
+		  homeGoals: 2,
+		  matchDate: 1440255600000,
+		  round: 3,
+		  referee: 'Neil Swarbrick'
+		},
+		{
+		  id: 7126,
+		  attendance: 39198,
+		  awayClub: 'Newcastle United',
+		  awayGoals: 0,
+		  homeClub: 'Manchester United',
+		  homeGoals: 0,
+		  matchDate: 1440255600000,
+		  round: 3,
+		  referee: 'Neil Swarbrick'
+		},
+		{
+		  id: 7127,
+		  attendance: 39198,
+		  awayClub: 'Manchester City',
+		  awayGoals: 2,
+		  homeClub: 'Everton',
+		  homeGoals: 0,
+		  matchDate: 1440345600000,
+		  round: 3,
+		  referee: 'Neil Swarbrick'
+		},
+		{
+		  id: 7128,
+		  attendance: 39198,
+		  awayClub: 'Southampton',
+		  awayGoals: 0,
+		  homeClub: 'Watford FC',
+		  homeGoals: 0,
+		  matchDate: 1440345600000,
+		  round: 3,
+		  referee: 'Neil Swarbrick'
+		},
+		{
+		  id: 7129,
+		  attendance: 39198,
+		  awayClub: 'Chelsea FC',
+		  awayGoals: 3,
+		  homeClub: 'West Brom',
+		  homeGoals: 2,
+		  matchDate: 1440336600000,
+		  round: 3,
+		  referee: 'Neil Swarbrick'
+		},
+		{
+		  id: 7130,
+		  attendance: 39198,
+		  awayClub: 'Liverpool FC',
+		  awayGoals: 0,
+		  homeClub: 'Arsenal',
+		  homeGoals: 0,
+		  matchDate: 1440446400000,
+		  round: 3,
+		  referee: 'Neil Swarbrick'
+		}
+	], 
+	[
+        {
+          id: 7131,
+          attendance: 39198,
+          awayClub: 'Crystal Palace',
+          awayGoals: 0,
+          homeClub: 'Chelsea FC',
+          homeGoals: 0,
+          matchDate: 1440446400000,
+          round: 4,
+          referee: 'Neil Swarbrick'
+        },
+        {
+          id: 7132,
+          attendance: 39198,
+          awayClub: 'Everton',
+          awayGoals: 0,
+          homeClub: 'Tottenham Hotspurs',
+          homeGoals: 0,
+          matchDate: 1440860400000,
+          round: 4,
+          referee: 'Neil Swarbrick'
+        },
+        {
+          id: 7133,
+          attendance: 39198,
+          awayClub: 'Leicester City',
+          awayGoals: 1,
+          homeClub: 'AFC Bournemouth',
+          homeGoals: 1,
+          matchDate: 1440860400000,
+          round: 4,
+          referee: 'Neil Swarbrick'
+        },
+        {
+          id: 7134,
+          attendance: 39198,
+          awayClub: 'Sunderland AFC',
+          awayGoals: 2,
+          homeClub: 'Aston Villa',
+          homeGoals: 2,
+          matchDate: 1440860400000,
+          round: 4,
+          referee: 'Neil Swarbrick'
+        },
+        {
+          id: 7135,
+          attendance: 39198,
+          awayClub: 'Watford FC',
+          awayGoals: 0,
+          homeClub: 'Manchester City',
+          homeGoals: 2,
+          matchDate: 1440860400000,
+          round: 4,
+          referee: 'Neil Swarbrick'
+        },
+        {
+          id: 7136,
+          attendance: 39198,
+          awayClub: 'West Brom',
+          awayGoals: 1,
+          homeClub: 'Stoke City',
+          homeGoals: 0,
+          matchDate: 1440860400000,
+          round: 4,
+          referee: 'Neil Swarbrick'
+        },
+        {
+          id: 7137,
+          attendance: 39198,
+          awayClub: 'West Ham',
+          awayGoals: 3,
+          homeClub: 'Liverpool FC',
+          homeGoals: 0,
+          matchDate: 1440860400000,
+          round: 4,
+          referee: 'Neil Swarbrick'
+        },
+        {
+          id: 7138,
+          attendance: 39198,
+          awayClub: 'Arsenal',
+          awayGoals: 1,
+          homeClub: 'Newcastle United',
+          homeGoals: 0,
+          matchDate: 1440852300000,
+          round: 4,
+          referee: 'Neil Swarbrick'
+        },
+        {
+          id: 7139,
+          attendance: 39198,
+          awayClub: 'Manchester United',
+          awayGoals: 1,
+          homeClub: 'Swansea City',
+          homeGoals: 2,
+          matchDate: 1440950400000,
+          round: 4,
+          referee: 'Neil Swarbrick'
+        },
+        {
+          id: 7140,
+          attendance: 39198,
+          awayClub: 'Norwich City',
+          awayGoals: 0,
+          homeClub: 'Southampton',
+          homeGoals: 3,
+          matchDate: 1440941400000,
+          round: 4,
+          referee: 'Neil Swarbrick'
+        }
+    ],
+    [
+        {
+          id: 7141,
+          attendance: 39198,
+          awayClub: 'Liverpool FC',
+          awayGoals: 1,
+          homeClub: 'Manchester United',
+          homeGoals: 3,
+          matchDate: 1442079000000,
+          round: 5,
+          referee: 'Neil Swarbrick',
+          venue: 6001
+        },
+        {
+          id: 7142,
+          attendance: 39198,
+          awayClub: 'Stoke City',
+          awayGoals: 0,
+          homeClub: 'Arsenal',
+          homeGoals: 2,
+          matchDate: 1442070000000,
+          round: 5,
+          referee: 'Neil Swarbrick',
+          venue: 6002
+        },
+        {
+          id: 7143,
+          attendance: 39198,
+          awayClub: 'Manchester City',
+          awayGoals: 1,
+          homeClub: 'Crystal Palace',
+          homeGoals: 0,
+          matchDate: 1442070000000,
+          round: 5,
+          referee: 'Neil Swarbrick',
+          venue: 6006
+        },
+        {
+          id: 7144,
+          attendance: 27018,
+          awayClub: 'AFC Bournemouth',
+          awayGoals: 1,
+          homeClub: 'Norwich City',
+          homeGoals: 3,
+          matchDate: 1442070000000,
+          round: 5,
+          referee: 'Martin Atkinson',
+          venue: 'Carrow Road'
+        },
+        {
+          id: 7145,
+          attendance: 20057,
+          awayClub: 'Swansea City',
+          awayGoals: 0,
+          homeClub: 'Watford FC',
+          homeGoals: 1,
+          matchDate: 1442070000000,
+          round: 5,
+          referee: 'Robert Madley',
+          venue: 'Vicarage Road'
+        },
+        {
+          id: 7146,
+          attendance: 24265,
+          awayClub: 'Southampton',
+          awayGoals: 0,
+          homeClub: 'West Brom',
+          homeGoals: 0,
+          matchDate: 1442070000000,
+          round: 5,
+          referee: 'Stuart Attwell',
+          venue: 'The Hawthorns'
+        },
+        {
+          id: 7147,
+          attendance: 38311,
+          awayClub: 'Chelsea FC',
+          awayGoals: 1,
+          homeClub: 'Everton',
+          homeGoals: 3,
+          matchDate: 1442061900000,
+          round: 5,
+          referee: 'Andre Marriner',
+          venue: 'Goodison Park'
+        },
+        {
+          id: 7148,
+          attendance: 40303,
+          awayClub: 'Tottenham Hotspurs',
+          awayGoals: 1,
+          homeClub: 'Sunderland AFC',
+          homeGoals: 0,
+          matchDate: 1442151000000,
+          round: 5,
+          referee: 'Craig Pawson',
+          venue: 'Stadium of Light'
+        },
+        {
+          id: 7149,
+          attendance: 31733,
+          awayClub: 'Aston Villa',
+          awayGoals: 2,
+          homeClub: 'Leicester City',
+          homeGoals: 3,
+          matchDate: 1442160000000,
+          round: 5,
+          referee: 'Mike Dean',
+          venue: 'King Power Stadium'
+        },
+        {
+          id: 7150,
+          attendance: 34907,
+          awayClub: 'Newcastle United',
+          awayGoals: 0,
+          homeClub: 'West Ham',
+          homeGoals: 2,
+          matchDate: 1442260800000,
+          round: 5,
+          referee: 'Anthony Taylor',
+          venue: 'Boleyn Ground'
+        }
+    ],
+    [
+	   {
+	      id: 7151,
+	      attendance: 41584,
+	      awayClub: 'Arsenal',
+	      awayGoals: 0,
+	      homeClub: 'Chelsea FC',
+	      homeGoals: 2,
+	      matchDate: 1442666700000,
+	      round: 6,
+	      referee: 'Mike Dean',
+	      venue: 'Stamford Bridge'
+	    },
+	    {
+	      id: 7152,
+	      attendance: 20805,
+	      awayClub: 'Everton',
+	      awayGoals: 0,
+	      homeClub: 'Swansea City',
+	      homeGoals: 0,
+	      matchDate: 1442674800000,
+	      round: 6,
+	      referee: 'Stuart Attwell',
+	      venue: 'Liberty Stadium'
+	    },
+	    {
+	      id: 7153,
+	      attendance: 27642,
+	      awayClub: 'Leicester City',
+	      awayGoals: 2,
+	      homeClub: 'Stoke City',
+	      homeGoals: 2,
+	      matchDate: 1442674800000,
+	      round: 6,
+	      referee: 'Andre Marriner',
+	      venue: 'Britannia Stadium'
+	    },
+	    {
+	      id: 7154,
+	      attendance: 47806,
+	      awayClub: 'Watford FC',
+	      awayGoals: 2,
+	      homeClub: 'Newcastle United',
+	      homeGoals: 1,
+	      matchDate: 1442674800000,
+	      round: 6,
+	      referee: 'Roger East',
+	      venue: 'St. James Park'
+	    },
+	    {
+	      id: 7155,
+	      attendance: 11271,
+	      awayClub: 'Sunderland AFC',
+	      awayGoals: 0,
+	      homeClub: 'AFC Bournemouth',
+	      homeGoals: 2,
+	      matchDate: 1442674800000,
+	      round: 6,
+	      referee: 'Kevin Friend',
+	      venue: 'Vitality Stadium'
+	    },
+	    {
+	      id: 7156,
+	      attendance: 36321,
+	      awayClub: 'West Brom',
+	      awayGoals: 1,
+	      homeClub: 'Aston Villa',
+	      homeGoals: 0,
+	      matchDate: 1442674800000,
+	      round: 6,
+	      referee: 'Martin Atkinson',
+	      venue: 'Villa Park'
+	    },
+	    {
+	      id: 7157,
+	      attendance: 53545,
+	      awayClub: 'West Ham',
+	      awayGoals: 2,
+	      homeClub: 'Manchester City',
+	      homeGoals: 1,
+	      matchDate: 1442683800000,
+	      round: 6,
+	      referee: 'Robert Madley',
+	      venue: 'Etihad Stadium'
+	    },
+	    {
+	      id: 7158,
+	      attendance: 35723,
+	      awayClub: 'Crystal Palace',
+	      awayGoals: 0,
+	      homeClub: 'Tottenham Hotspurs',
+	      homeGoals: 1,
+	      matchDate: 1442755800000,
+	      round: 6,
+	      referee: 'Robert Madley',
+	      venue: 'White Hart Lane'
+	    },
+	    {
+	      id: 7159,
+	      attendance: 31588,
+	      awayClub: 'Manchester United',
+	      awayGoals: 3,
+	      homeClub: 'Southampton',
+	      homeGoals: 2,
+	      matchDate: 1442764800000,
+	      round: 6,
+	      referee: 'Mark Clattenburg',
+	      venue: 'St. Marys Stadium'
+	    },
+	    {
+	      id: 7160,
+	      attendance: 44072,
+	      awayClub: 'Norwich City',
+	      awayGoals: 1,
+	      homeClub: 'Liverpool FC',
+	      homeGoals: 1,
+	      matchDate: 1442764800000,
+	      round: 6,
+	      referee: 'Anthony Taylor',
+	      venue: 'Anfield'
+	    }
+	  ]
+];
